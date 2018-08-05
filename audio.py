0e1c479d95b5e0883244ef59391c7c496aa57db8
xMoha <40671043+xMoha@users.noreply.github.com>
2018-08-05 10:47:42 +0300

    Create audio.py

---
+++ audio.py
@@ -0,0 +1,83 @@
+import discord
+import asyncio
+import youtube_dl
+import os
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+
+bot=commands.Bot(command_prefix='X')
+
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+load_opus_lib()
+
+
+
+@bot.event
+async def on_ready():
+    print("hi")
+opts = {
+            'default_search': 'auto',
+            'quiet': True,
+        }    
+    
+@bot.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+
+
+
+players={}
+@bot.command(pass_context=True)
+async def play(ctx, *,url):
+    global play_server
+    play_server = ctx.message.server
+    voice = bot.voice_client_in(play_server)
+    global player
+    player = await voice.create_ytdl_player(url,ytdl_options=opts)
+    players[play_server.id] = player
+    if player.is_live == True:
+        await bot.say("Can not play live audio yet.")
+    elif player.is_live == False:
+        player.start()
+
+
+async def pause(ctx):
+    player.pause()
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    player.resume()
+          
+@bot.command(pass_context=True)
+async def volume(ctx, vol):
+    vol = float(vol)
+    vol = player.volume = vol
+
+@bot.command(pass_context=True)
+async def stop(ctx):
+    server=ctx.message.server
+    voice_client=bot.voice_client_in(server)
+    await voice_client.disconnect()
+
+
+
+bot.run(os.environ['BOT_TOKEN'])
